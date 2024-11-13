34c99e8ab577b23cd8d9cb174eb9ecdbde69375f
Tejas Shah <tejasshah2k19@gmail.com>
2024-11-13 21:12:25 +0530

    repo

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -1,13 +1,34 @@
 package com.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.entity.UserEntity;
+import com.repository.UserRepository;
 
 @Controller
 public class UserController {
+
+	@Autowired
+	UserRepository userRepository; 
 	
 	@GetMapping("/newuser")
 	public String newUser() {
 		return "NewUser";// jsp name
+	}
+
+	@PostMapping("/createUser")
+	public String createUser(UserEntity userEntity) {
+		// how to read data here -> jsp form data in controller?
+		// bean class==entity class 
+		
+		//read ? 
+		//save user info into database 
+		
+		//repository -> query 
+		userRepository.save(userEntity);
+		return "NewUser";
 	}
 }
